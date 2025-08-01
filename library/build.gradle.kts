eb8a3f0d5b0c3a39edff01bd12a58157666ac4dd
Thonsi <10291398+thonsi@users.noreply.github.com>
2025-08-01 08:32:39 +0530

    1

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -26,32 +26,32 @@ android {
 afterEvaluate {
     publishing {
         publications {
-            create<MavenPublication>("release") {
-                from(components.findByName("release"))
+           create<MavenPublication>("release") {
+                from(components["release"])
 
                 group = "com.github.thonsi"
                 artifactId = "android-spinkit"
                 version = "1.0.4"
 
                 pom {
-                    name.set("Android-SpinKit")
-                    description.set("SpinKit for Android application")
+                name.set("Android-SpinKit")
+                description.set("SpinKit for Android application")
+                url.set("https://github.com/thonsi/android-spinkit")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        name.set("thonsi")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/thonsi/android-spinkit.git")
+                    developerConnection.set("scm:git:ssh://git@github.com/thonsi/android-spinkit.git")
                     url.set("https://github.com/thonsi/android-spinkit")
-                    licenses {
-                        license {
-                            name.set("The Apache License, Version 2.0")
-                            url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
-                        }
-                    }
-                    developers {
-                        developer {
-                            name.set("thonsi")
-                        }
-                    }
-                    scm {
-                        connection.set("scm:git:https://github.com/thonsi/android-spinkit")
-                        url.set("https://github.com/thonsi/android-spinkit")
-                    }
                 }
             }
         }
